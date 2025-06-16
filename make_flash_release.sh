a844463f7851617d8cfe215b9078ab399289b3f2
Shantanu Ghodgaonkar <sghodgaonkar@botcrew.com>
2025-06-16 14:20:36 -0500

    Flash over CAN script cleanup

--- make_flash_release.sh
+++ make_flash_release.sh
@@ -1,22 +1,3 @@
-# #!/bin/bash
-# set -e  # Exit if any command fails
-
-# # Step 1: Make the release
-# echo "Creating release..."
-# ./make_release.py
-
-# # Step 2: Flash the latest .elf file
-# echo "Flashing..."
-# ELF_FILE=$(ls ./release/*-moteus-*.elf | head -n 1)
-# if [ -z "$ELF_FILE" ]; then
-#   echo "Error: No ELF file found to flash."
-#   exit 1
-# fi
-
-# python3 -m moteus.moteus_tool --target 1 --flash "$ELF_FILE"
-
-
-
 #!/bin/bash
 set -e  # Exit on any error
 
